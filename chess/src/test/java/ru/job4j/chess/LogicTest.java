02e06511c2f250650b190b3d4cf3a9284dc9f381
Dmitriy Kupreev <d3_october@yahoo.com>
2023-03-14 11:48:37 +0300

    3. Каркас шахматной доски [#792 #400507]_4

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -10,21 +10,24 @@ public class LogicTest {
     @Test
     public void whenMoveThenFigureNotFoundException() {
         Logic logic = new Logic();
-        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> logic.move(Cell.C1, Cell.H6));
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class,
+                () -> logic.move(Cell.C1, Cell.H6));
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
     }
 
 //    @Test
 //    public void whenMoveThenImpossibleMoveException() {
 //        Logic logic = new Logic();
-//        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> logic.move(Cell.C1, Cell.C6));
+//        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class,
+//                () -> logic.move(Cell.C1, Cell.C6));
 //        assertThat(exception.getMessage()).isEqualTo("Could not move by diagonal from C1 to C6.");
 //    }
 //
 //    @Test
 //    public void whenMoveThenOccupiedCellException() {
 //        Logic logic = new Logic();
-//        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> logic.move(Cell.C1, Cell.C1));
+//        OccupiedCellException exception = assertThrows(OccupiedCellException.class,
+//                () -> logic.move(Cell.C1, Cell.C1));
 //        assertThat(exception.getMessage()).isEqualTo("Occupied cell on the board.");
 //    }
 }
